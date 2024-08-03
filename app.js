96a95f66312acce77e7059d1ccfb5d94f60b3de2
kareem ahmed <kareemahmed252004@gmail.com>
2024-08-04 00:40:02 +0300

    "kareem"

--- app.js
+++ app.js
@@ -1,27 +1,74 @@
-require('dotenv').config();
 const express = require('express');
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-
+const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 8080;
+const session = require('express-session');
+const existingIds = new Set(); 
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); // Use the 'views' directory
+function generateUniqueId() {
+    let id;
+    do {
+        id = [...Array(32)].map(() => Math.random().toString(36)[2]).join('');
+    } while (existingIds.has(id)); 
+    existingIds.add(id); 
+    return id;
+}
 
-app.use(expressLayouts);
-app.set('layout', 'layouts/main'); // Layout path relative to 'views'
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(session({
+    genid: (req) => {
+        return uuidv4();
+    },
+    secret: 'your-secret-key',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-app.get('/', (req, res) => {
-    res.send("hello world");
+class user{
+    constructor(username , password){
+        this.username = username;
+        this.password = password;
+        this.id = id;
+    }
+}
+const arr = [new user('maged', 'maged')];
+function search(id){
+for(i = 0;i < arr.length ;i++){
+    if(id==arr[i].id){
+        return arr[i];
+    }
+}
+return null;
+}
+function managesessions(req,set){
+    if (!req.session.user && arr.length!=0 ) {
+       
+
+        
+        console.log("new client connected");
+        if(set){
+        req.session.user = generateUniqueId();
+        req.session.user = arr.push.use
+        res.send('maged');
+        }else{
+            return req.session.user;
+        }
+        return 'not found';
+    } else {
+        let currentuser = search(req.session.user);
+        if(currentuser!=nullptr){
+        console.log("reconnected", req.session.user);
+        res.send('welcome back ' + req.session.user);
+        return req.session.user;
+        }}
+};
+
+app.get('/maged', (req, res) => {
+    
 });
 
-app.get('/account', (req, res) => {
-    res.render('account', { title: 'Account Page' });
-});
 
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
